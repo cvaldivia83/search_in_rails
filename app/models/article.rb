ac7cc682e997fb98fbb6867379a08bdab0601edb
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 14:56:25 -0300

    Add validations in title, subtitle, author length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :author, :body, presence: true
-  
+  validates :title, :subtitle, length: { maximum: 80 }
+  validates :body, length: { maximum: 20000 }
+  validates :author, length: { minimum: 2 }
+
 end
