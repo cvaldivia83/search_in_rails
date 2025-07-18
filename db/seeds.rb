103d67fbbec743af3203dab739016fdd043dd453
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 17:18:10 -0300

    [feat] Add seeds for 8 articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'Faker'
+
+Article.destroy_all
+
+categories = %w(tech career challenge tutorial hackathon)
+
+authors = ["Carla Valdivia", "Jorge Cabrera", "André Digiorno"]
+
+puts "Creating 8 articles..."
+
+8.times do |i|
+  article = Article.new(
+    title: Faker::Lorem.sentence(word_count: rand(7..9)),
+    subtitle: Faker::Lorem.sentence(word_count: rand(5..9)),
+    category: categories.sample,
+    author: authors.sample,
+    body: Faker::Lorem.paragraphs(number: rand(20..30)).join
+  )
+
+  article.save!
+
+  puts "Created article n. #{i + 1}."
+end
