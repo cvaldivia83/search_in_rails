f1ab0b8628d919fe44fee8db46039c00b5579b3a
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 17:47:00 -0300

    [test] Add search_item factory and search_item validations spec

--- spec/factories/search_items.rb
+++ spec/factories/search_items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :search_item do
-    user_ip { "MyString" }
-    query { "MyString" }
+    user_ip { "99.149.108.239" }
+    query { "Why is the sky blue?" }
   end
 end
--- spec/models/search_item_spec.rb
+++ spec/models/search_item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SearchItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+
+    it "Search Item is not valid without a user_ip" do 
+      search = build(:search_item, user_ip: nil)
+      expect(search).not_to be_valid
+    end
+
+    it "Search Item is not valid without a query" do 
+      search = build(:search_item, query: nil)
+      expect(search).not_to be_valid
+    end
+    it "Search Item is valid with user_ip and query attributes" do 
+      search_item = build(:search_item)
+      expect(search_item).to be_valid
+    end
+
+    it "Search Item has a valid IP address format" do 
+      search_item = build(:search_item)
+      expect(search_item).to be_valid
+    end
+
+    it "Search Item does not have a valid address format" do 
+      search_item = build(:search_item, user_ip: "232191175117")
+      expect(search_item).not_to be_valid
+    end
+  end
 end
