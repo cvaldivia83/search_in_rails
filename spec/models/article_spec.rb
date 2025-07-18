9ed7ded9c01693a992c3f1521e72acdde6fc6750
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 15:26:42 -0300

    [test] Add specs for article model title, subtitle, body, author length validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe "validations" do 
-    it "creates an instance of Article with valid attributes" do
-      article = build(:article)
-      expect(article).to be_valid
-    end
-    
     it "Article is not valid without a title" do 
       article = build(:article, title: nil)
+      expect(article).not_to be_valid
+    end
+
+    it "Article is not valid without a subtitle" do
+      article = build(:article, subtitle: nil)
       expect(article).not_to be_valid
     end
 
@@ -23,18 +23,28 @@ RSpec.describe Article, type: :model do
     end
 
     it "Article's title and subtitle should not exceed 90 characters length" do
-      article = build(:article, title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum culpa perferendis qui exercitationem', subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum culpa perferendis qui exercitationem')
+      article = build(:article, :long_title, :long_subtitle)
       expect(article).not_to be_valid
     end
 
-    it "Article's body should not exceed 1_500 characters in length" do 
-      article = build(:article)
+    it "Article's body should not exceed 2_000 characters in length" do 
+      article = build(:article, :long_body)
+      expect(article).not_to be_valid
+    end
+
+    it "Article's body should have at least 800 characters in length" do 
+      article = build(:article, :short_body)
       expect(article).not_to be_valid
     end
 
     it "Article's author should contain at least 2 characters in length" do
       article = build(:article, author: "Y")
       expect(article).not_to be_valid
+    end
+
+    it "creates a valid Article with all attributes in correct length" do 
+      article = build(:article, :correct_body)
+      expect(article).to be_valid
     end
 
   end
