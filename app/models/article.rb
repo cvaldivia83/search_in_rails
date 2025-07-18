3d0b3da5c2a7aa281b32f5a85b4e6fdcdd715e09
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 15:49:12 -0300

    [test] Add validation for category and uniqueness

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   validates :title, :subtitle, :author, :body, presence: true
+  validates :title, :subtitle, :body, uniqueness: true
+  validates :category, inclusion: { in: %w(tech career challenge tutorial hackathon), message: "This is not a valid category."}
   validates :title, :subtitle, length: { maximum: 80 }
   validates :body, length: { maximum: 2000, minimum: 800 }
   validates :author, length: { minimum: 2 }
-
 end
