f82a1f3da5584851576d95f2fd8a914cd990e18b
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-18 15:27:02 -0300

    Add validations for article model for length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
-  validates :title, :author, :body, presence: true
+  validates :title, :subtitle, :author, :body, presence: true
   validates :title, :subtitle, length: { maximum: 80 }
-  validates :body, length: { maximum: 20000 }
+  validates :body, length: { maximum: 2000, minimum: 800 }
   validates :author, length: { minimum: 2 }
 
 end
