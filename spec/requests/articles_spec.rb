5093ff072df1b871aa7ece0bf2a065e2c9d1fb03
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-19 04:53:33 -0300

    [test] Add test for new articles controller

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :request do
+RSpec.describe "Articles Controller", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    it "renders the correct template for root path" do 
+      get root_path
+      expect(response).to render_template(:index)
+    end
+
+    it "fetches all articles from DB and assigns it to @articles" do
+      article1 = create(:article, :correct_body)
+      get root_path
+      expect(assigns(:articles)).to match_array([article1])
+    end
+
+    it "searches for the correct Article" do
+      # article = create(:article, :correct_body)
+      
+      # get root_path, params: { query: "lorem ipsum" }
+    end
   end
 end
