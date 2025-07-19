f1f949b2655587bb7c48678e7218b84de758a4e9
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-19 04:56:37 -0300

    [test] Removed unused test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -13,11 +13,5 @@ RSpec.describe "Articles Controller", type: :request do
       get root_path
       expect(assigns(:articles)).to match_array([article1])
     end
-
-    it "searches for the correct Article" do
-      # article = create(:article, :correct_body)
-      
-      # get root_path, params: { query: "lorem ipsum" }
-    end
   end
 end
