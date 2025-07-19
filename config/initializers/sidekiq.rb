c08f600ca5744bf9fe162f8f38c71d8b76de2168
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-19 12:36:45 -0300

    [fix] sidekiq redisgreen config for heroku production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+redis_url = ENV['REDISGREEN_URL'] || ENV['REDIS_URL'] || 'redis://localhost:6379/0'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  config.redis = { url: redis_url }
 end
