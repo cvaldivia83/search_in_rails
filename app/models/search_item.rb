5a57e7a22616d21c85f58becf226d5a7cc696971
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-19 01:56:04 -0300

    [fix] uncomment ip format validation

--- app/models/search_item.rb
+++ app/models/search_item.rb
@@ -1,4 +1,4 @@
 class SearchItem < ApplicationRecord
   validates :query, :user_ip, presence: true
-  # validates :user_ip, format: { with: /(\d{1,3}\.){3}\d{1,3}/, message: "Invalid IP address format"}
+  validates :user_ip, format: { with: /(\d{1,3}\.){3}\d{1,3}/, message: "Invalid IP address format"}
 end
