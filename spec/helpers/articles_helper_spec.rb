7f296b38e6f80ed13977648e1b8d411d29316fad
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-20 18:01:05 -0300

    [test] Add spec to articles_helper method render_category

--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -11,5 +11,35 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ArticlesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#render_category" do 
+    it "returns indigo color utility for tech article category" do  
+      results = render_category("tech")
+      expect(results).to match("bg-indigo-200 text-indigo-800")
+    end
+
+    it "returns red color utility for career article category" do 
+      results = render_category('career')
+      expect(results).to match("bg-red-200 text-red-800")
+    end
+
+    it "returns purple color utility for challenge article category" do 
+      results = render_category('challenge')
+      expect(results).to match('bg-purple-200 text-purple-800')
+    end
+
+    it "returns pink color utility for tutorial article category" do 
+      results = render_category('tutorial')
+      expect(results).to match('bg-pink-200 text-pink-800')
+    end
+
+    it "returns teal color utility for hackathon article category" do 
+      results = render_category('hackathon')
+      expect(results).to match('bg-teal-200 text-teal-800')
+    end
+
+    it "if a new category opens up it should return a slate color utility" do 
+      results = render_category('code')
+      expect(results).to match('bg-slate-200 text-slate-800')
+    end
+  end
 end
