273265314c2475026f34341d1d8ce080694278d1
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-20 21:08:30 -0300

    [test] Add spec to test SearchItemJob

--- spec/jobs/search_item_job_spec.rb
+++ spec/jobs/search_item_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe SearchItemJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "# SearchItem background jobs" do
+    it "inserts a background job to a queue" do 
+      expect {
+        SearchItemJob.perform_later("why is the sky blue?", "192.75.230.81")
+      }.to have_enqueued_job(SearchItemJob)
+    end
+    
+    it "checks if background job performs with passed arguments" do 
+      expect {
+        SearchItemJob.perform_later("learn ruby in 2 weeks", "109.72.801.62")
+      }.to have_enqueued_job.with("learn ruby in 2 weeks", "109.72.801.62")
+    end
+
+    it "uses a background to create a search_item instance" do 
+      expect {
+        SearchItemJob.perform_now("Who is Pierre Bourdieu?", "188.09.482.71")
+      }.to change(SearchItem, :count).by(1)
+
+      search_item = SearchItem.last
+      expect(search_item.query).to eq("Who is Pierre Bourdieu?")
+      expect(search_item.user_ip).to eq("188.09.482.71")
+    end
+  end
 end
